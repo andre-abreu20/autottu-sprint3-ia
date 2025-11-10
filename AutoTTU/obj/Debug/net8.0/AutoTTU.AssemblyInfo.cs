b0277b96ae599dc7cc6fed2bb0729d654c4cd189
andre-abreu20 <andre.luismesquita@hotmail.com>
2025-11-09 21:49:53 -0300

    readme melhorado

--- AutoTTU/obj/Debug/net8.0/AutoTTU.AssemblyInfo.cs
+++ AutoTTU/obj/Debug/net8.0/AutoTTU.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoTTU")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+247e77513ba07ace6a3e01d56a0a4b8287832a43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8cd8690ebab92d9a5d913d5508692d3df7f381c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoTTU")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoTTU")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
